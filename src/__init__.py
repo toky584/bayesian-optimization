2ab0eb06bf567460deb874b28422a138785129cc
toky584 <toky@aims.ac.za>
2025-03-01 21:41:00 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from jax import random
+import jax.numpy as jnp
+import numpyro
+import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS
